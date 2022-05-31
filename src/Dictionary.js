6f0a00effe8f55ddfc51cb43d4a9d915794753b7
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-05-31 17:04:37 +0100

    added reference to website api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,7 @@ export default function Dictionary() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
-
+  // documentation: https://dictionaryapi.dev/
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
