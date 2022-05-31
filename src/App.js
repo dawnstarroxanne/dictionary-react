588ca6e38966cd340843da26b2b6164c9fc87b87
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-05-31 14:13:53 +0100

    added new component and functions

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>coded by Dawn Roxanne</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
