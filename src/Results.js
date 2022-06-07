e85cbf8140e4edb187a0a3d54ced1322774227c6
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-06-07 15:20:37 +0100

    set a default keyword

--- src/Results.js
+++ src/Results.js
@@ -1,25 +1,27 @@
 import React from "react";
 import Meanings from "./Meanings";
 import Phonetics from "./Phonetics";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetics phonetic={phonetic} />
-            </div>
-          );
-        })}
-
+        <section>
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetics phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <section key={index}>
               <Meanings meaning={meaning} />
-            </div>
+            </section>
           );
         })}
       </div>
